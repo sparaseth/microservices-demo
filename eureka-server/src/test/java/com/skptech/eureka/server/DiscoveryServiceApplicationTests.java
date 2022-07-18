bbf845941c1bb9cada09b87765ee5e6da448558c
sparaseth <sparaseth@gmail.com>
2022-07-19 01:00:51 +0530

    Initial Commit - Eureka Server

---
+++ eureka-server/src/test/java/com/skptech/eureka/server/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skptech.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
